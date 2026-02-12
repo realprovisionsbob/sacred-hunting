915cd01422008e86298a599768b28c75562cf53e
Isaac Lien <isaaclien@Isaacs-Mac-mini.local>
2026-02-12 10:29:28 -0800

    Fix: scroll to top on page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop'
 import Navigation from './components/Navigation'
 import Footer from './components/Footer'
 import HomePage from './pages/HomePage'
@@ -13,6 +14,7 @@ import NotFoundPage from './pages/NotFoundPage'
 function App() {
   return (
     <div className="min-h-screen bg-earth-dark">
+      <ScrollToTop />
       <Navigation />
       <main>
         <Routes>
